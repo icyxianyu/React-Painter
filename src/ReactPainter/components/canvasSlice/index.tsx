9e3d99ee20f73ffcd2f9d8663e82260336e07df8
yanjingyu <yanjingyu@gs-robot.com>
2023-03-02 22:48:11 +0800

    fix: 下一版全面推翻重构,前置版本作废

---
+++ src/ReactPainter/components/canvasSlice/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CanvasSlice() {
+  return (
+    <div>
+        canvasSlice
+    </div>
+  )
+}
--- src/ReactPainter/index.tsx
+++ src/ReactPainter/index.tsx
@@ -6,109 +6,121 @@ import TopToolBar from './components/TopToolBar/TopToolBar'
 import { DrawImg, keyPressAction } from './utils';
 export default function ReactPinter() {
     const [history,setHistory] =useState<any>([null]);
-    const [canvas,setCanvas] = useState<HTMLElement|any>();
-    const [ctx,setCtx] = useState<any>();
+    const [canvas,setCanvas] = useState<HTMLElement|any>([]);
     const [isClick,setIsClick] = useState<boolean>(false);
     const [Event,setEvent] = useState<any>(); 
-    const addToHistory = ()=>{
-        setHistory([...history,{src:canvas.toDataURL(),
-                    action:"painDraw"}]
+    const addToHistory = (canvasInstance:any,ctx:any)=>{
+        setHistory([...history,
+                    {
+                        src:canvasInstance.toDataURL(),
+                        action:"painDraw",
+                        canvas:canvasInstance,
+                        ctx
+                    }]
         .slice(-50))
     }
-    const onMousedown = (event:any) => {
+    useEffect(()=>{
+        const date = Date.now();
+        setCanvas([date]);
+    },[])
+    const onMousedown = (event:any,canvasInstance:any,ctx:any) => {
         if (event.button === 0) {
-            const offsetX = event.clientX - canvas.getBoundingClientRect().left;
-            const offsetY = event.clientY - canvas.getBoundingClientRect().top;
-            console.log(offsetX,offsetY)
+            const offsetX = event.clientX 
+                            - canvasInstance.getBoundingClientRect().left;
+            const offsetY = event.clientY 
+                            - canvasInstance.getBoundingClientRect().top;
             ctx.beginPath();
             ctx.moveTo(offsetX, offsetY);
             setIsClick(true);
         }
     }
-    const onMouseMove = (event:any) => {
+    const onMouseMove = (event:any,canvasInstance:any,ctx:any) => {
         if(isClick){
-            const offsetX = event.clientX - canvas.getBoundingClientRect().left;
-            const offsetY = event.clientY - canvas.getBoundingClientRect().top;
+            const offsetX = event.clientX 
+                            - canvasInstance.getBoundingClientRect().left;
+            const offsetY = event.clientY 
+                            - canvasInstance.getBoundingClientRect().top;
             ctx.lineTo(offsetX,offsetY);
             ctx.stroke();
         }
     }
-    const onMouseUp = () => {
+    const onMouseUp = (event:any,canvasInstance:any,ctx:any) => {
         setIsClick(false);
-        addToHistory();
+        addToHistory(canvasInstance,ctx);
     }
-    const changeInput = (name:string,value:any) =>{
-        console.log(name,value)
-        switch (name){
-            case "backgroundcolor" :
-                canvas.style.backgroundColor = value;
-                break;
-            case "paintcolor":
-                ctx.strokeStyle=value;
-                break;
-            case "paintfont" :
-                ctx.lineWidth = value;
-                break;
-        }
-    }
-    const clear = ()=>{
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-    }
-    const save = () =>{
+    // const changeInput = (name:string,value:any) =>{
+    //     switch (name){
+    //         case "backgroundcolor" :
+    //             canvas.style.backgroundColor = value;
+    //             break;
+    //         case "paintcolor":
+    //             ctx.strokeStyle=value;
+    //             break;
+    //         case "paintfont" :
+    //             ctx.lineWidth = value;
+    //             break;
+    //     }
+    // }
+    // const clear = ()=>{
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // }
+    // const save = () =>{
 
-        // 创建一个 a 标签，并设置 href 和 download 属性
-        const el = document.createElement('a');
-        // 设置 href 为图片经过 base64 编码后的字符串，默认为 png 格式
-        el.href = canvas.toDataURL();
-        el.download = '图片';
-        // 创建一个点击事件并对 a 标签进行触发
-        const event = new MouseEvent('click');
-        el.dispatchEvent(event);
-    }
-    const keydown=(event:any)=>{
-        event.preventDefault();
-        event.stopPropagation();
-        setEvent(event)
-    }
-    const changeHistory = (index:number) =>{
-        DrawImg({ctx,canvas,history},setHistory,index)
-    }
-    const replay = () =>{
+    //     // 创建一个 a 标签，并设置 href 和 download 属性
+    //     const el = document.createElement('a');
+    //     // 设置 href 为图片经过 base64 编码后的字符串，默认为 png 格式
+    //     el.href = canvas.toDataURL();
+    //     el.download = '图片';
+    //     // 创建一个点击事件并对 a 标签进行触发
+    //     const event = new MouseEvent('click');
+    //     el.dispatchEvent(event);
+    // }
+    // const keydown=(event:any)=>{
+    //     event.preventDefault();
+    //     event.stopPropagation();
+    //     setEvent(event)
+    // }
+    // const changeHistory = (index:number) =>{
+    //     DrawImg({ctx,canvas,history},setHistory,index)
+    // }
+    // const replay = () =>{
 
-    }
-    useEffect(()=>{
-        if(Event){
-            keyPressAction(Event,canvas,ctx,history,setHistory)
-        }
-    },[Event,canvas,ctx])
-    useEffect(()=>{
-        if(canvas&&ctx){
-            document.addEventListener("keydown",keydown)
-        }
-        return ()=>{
-            document.removeEventListener("keydown",keydown)
-        }
-    },[canvas,ctx])
+    // }
+    // useEffect(()=>{
+    //     if(Event){
+    //         keyPressAction(Event,canvas,ctx,history,setHistory)
+    //     }
+    // },[Event])
+    // useEffect(()=>{
+    //         document.addEventListener("keydown",keydown)
+    //     return ()=>{
+    //         document.removeEventListener("keydown",keydown)
+    //     }
+    // },[canvas])
 
     
   return (
     <div style={{display:"flex",flexDirection:"column",height:"100%"}} >
-        <TopToolBar 
+        {/* <TopToolBar 
             clear={clear}
             save={save}
             changeInput={changeInput}
             replay={replay}
             history={history} 
-            changeHistory={changeHistory}/>
+            changeHistory={changeHistory}/> */}
         <div style={{display:"flex",flex:1}}>
             <LeftToolBar></LeftToolBar>
-            <Draw 
-                setCanvas = {setCanvas}
-                setCtx ={setCtx}
-                onMousedown={onMousedown}
-                onMouseMove={onMouseMove}
-                onMouseUp={onMouseUp}
-            />
+            {
+                canvas.map((item:any,index:number)=>{
+                    return <Draw 
+                        key={item}
+                        index={item}
+                        onMousedown={onMousedown}
+                        onMouseMove={onMouseMove}
+                        onMouseUp={onMouseUp}/>
+                })
+            }
+
             <RightToolBar></RightToolBar>
          </div>
     </div>
